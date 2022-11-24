d3325b3baad3d2df78d8d2d11035e137f9f51877
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-11-24 21:25:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0;
+    int fib=0;
+    int fib_2=1;
+    int somma=0;
+    cin>>n;
+        cout<<fib_2<<endl;
+        while(somma<=n){
+            somma=fib+fib_2;
+            fib=fib_2;
+            fib_2=somma;
+            if(somma<=n){
+                cout<<somma<<endl;
+            }
+        }
+    return 0;
 }
